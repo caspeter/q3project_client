1a3f51a1ff91809b8cbbd01ad2ec55f369aa2c0b
Evan Busse <evanbusse@gmail.com>
2017-01-24 14:48:48 -0700

    sorted out some styling issues

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,14 +1,14 @@
 'use strict';
 
 import React from 'react';
-import Card from 'material-ui/Card/Card';
+import Card from 'material-ui/Card';
 import CardActions from 'material-ui/Card/CardActions';
 import CardHeader from 'material-ui/Card/CardHeader';
 import CardMedia from 'material-ui/Card/CardMedia';
 import CardTitle from 'material-ui/Card/CardTitle';
 import CardText from 'material-ui/Card/CardText';
 import Avatar from 'material-ui/Avatar/Avatar';
-import Button from 'material-ui/RaisedButton/RaisedButton';
+import Button from 'material-ui/RaisedButton';
 
 
 const post = React.createClass({
@@ -20,16 +20,22 @@ const post = React.createClass({
   render() {
     return(
       <Card className="uiCard">
-        <CardTitle>
-          <h2 className="postTitle">Post Title</h2>
-        </CardTitle>
-        <CardText>
-          <p className="postUser">{this.props.postData.username}</p>
-          <p>{this.props.postData.description}</p>
-        </CardText>
+        <CardTitle
+          title="Post Title"
+          subtitle={this.props.postData.username}
+        />
+        <CardText
+          content={this.props.postData.username}
+        />
         <CardActions>
-          <Button className="postAction">Favorite Post</Button>
-          <Button className="postAction">Add Comment</Button>
+          <Button
+            label="Favorite Post"
+            primary='true'
+          />
+          <Button
+            label="View Comments"
+            primary="true"
+          />
         </CardActions>
       </Card>
     )
