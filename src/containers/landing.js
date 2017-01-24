affd8f926304159340c92c2e1c6b24d9f8682b4b
mittino <annamefford@gmail.com>
2017-01-24 10:58:15 -0700

    creating router.js and landing container

---
+++ src/containers/landing.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import request from 'superagent';
+import Login from './components/login.js';
+import Nav from './components/nav.js';
--- src/router.js
+++ src/router.js
@@ -2,5 +2,21 @@ import React from 'react';
 import {Router, Route, browserHistory, IndexRoute, Link } from 'react-router';
 
 
+//import containers:
+
+
 //import components:
 import login from "./components/login.js";
+
+var AppRouter = React.createClass({
+
+  render: function(){
+    return(
+      <Router history={browserHistory}>
+        <Route path="/" container={LandingPage} />
+      </Router>
+    )
+
+
+  }
+})
