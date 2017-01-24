aa193668724e33b570c0854690ea64f9d800d890
mittino <annamefford@gmail.com>
2017-01-23 18:16:32 -0700

    initial commit - moved over client side files

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import request from 'superagent';
+//import { connect } from 'react-redux';
+// import Comment from './Comment.js';
+// import Landing from './Landing.js';
+// import Nav from './Nav.js';
+// import NewPostForm from './NewPostForm.js';
+// import Post from './Post.js';
+// import PostsContainer from './PostsContainer';
+// import Profile from './Profile';
+
+const App = React.createClass({
+
+  // getInitialState(){
+  //   return state;
+  // },
+
+  componentDidUpdate(){
+    console.log('updated')
+  },
+
+  render(){
+    //<div>Test</div>;
+    return(
+      <div> TESTING </div>
+    );
+  }
+
+});
+
+export default App;
