ccca3d4dd71d21cbfeb5014db52a974341b53cd3
Evan Busse <evanbusse@gmail.com>
2017-01-26 11:47:48 -0700

    changed button color to green

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,11 +4,16 @@
 import React, {PropTypes} from 'react';
 import {Link, IndexLink} from 'react-router';
 import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
 
 // this header component gets referenced on the app.js file.  the Header component
 // is also imported on the app.js page
 const Nav = React.createClass({
   render: function(){
+    var buttonStyle = {
+      backgroundColor: 'transparent',
+      color: 'white'
+    };
     return(
 
      //using the indexLink component from react-router to handle this
@@ -17,23 +22,26 @@ const Nav = React.createClass({
      //and apply a class for me.  So you can style the currently selected anchor
      //the header
      <nav>
-
-         <RaisedButton
-            href="/"
-            label="Landing"
-            primary={true}
-          />
-         <RaisedButton
-            href="/myProfile"
-            label="My Profile"
-            primary={true}
-          />
-         <RaisedButton
-            href="/myProjects"
-            label="My Projects"
-            primary={true}
-          />
-
+      <div className="navWrapper">
+        <RaisedButton
+        className="navButton"
+        href="/"
+        label="Landing"
+        primary={true}
+        />
+        <RaisedButton
+        className="navButton"
+        href="/myProfile"
+        label="My Profile"
+        primary={true}
+        />
+        <RaisedButton
+        className="navButton"
+        href="/myProjects"
+        label="My Projects"
+        primary={true}
+        />
+      </div>
      </nav>
      // putting a pipe between the links to keep layout simple
    );
