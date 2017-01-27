827e69ba3dc29945e93a8f291f6031a8d137bc8a
Cassie Peterson <cas.j.peters@gmail.com>
2017-01-27 11:12:19 -0700

    client side communicating with server

--- src/containers/editprofile.js
+++ src/containers/editprofile.js
@@ -5,7 +5,7 @@ import { Router, browserHistory } from 'react-router'
 import routes from '../router'
 import Nav from '../components/Nav';
 
-const DATABASE_URL = "http://localhost:5000";
+const DATABASE_URL = "https://artops-server.herokuapp.com";
 
 var editProfileContainer = React.createClass({
 
